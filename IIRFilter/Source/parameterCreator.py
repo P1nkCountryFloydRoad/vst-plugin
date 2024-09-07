4cac749d17fe408c4622484b76d5158024faaad7
Bill Yang <billyang@BilldeMacBook-Air.local>
2024-09-07 10:16:01 +0800

    delete unnecessary

--- IIRFilter/Source/parameterCreator.py
+++
@@ -1,6 +0,0 @@
-from scipy.signal import butter
-
-# 设计带通滤波器，假设采样频率为 1000 Hz，带通频率为 100 Hz 到 300 Hz
-b, a = butter(N=1, Wn=[100, 880], btype='band', fs=44100)
-print("b coefficients: ", b)
-print("a coefficients: ", a)
